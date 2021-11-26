a39224ad392ba413970f9e8171e619ed3b1e641b
Justcallmeleej <95065498+Justcallmeleej@users.noreply.github.com>
2021-11-26 10:38:01 +0000

    Create config.js

---
+++ config.js
@@ -0,0 +1,20 @@
+const CONFIG = {
+    titleWeb: "bluemoon",
+    introTitle: 'You',
+    introDesc: `Hey you! Long time no see!
+    Thanks! You're awesome for being fond of me
+    It'll Be Great If We Are Friends
+    Friends...tks`,
+    btnIntro: '^^HiHi^^',
+    title: 'Just friends 🥰',
+    desc: 'Once, normal friends ',
+    btnYes: 'okelaa <33',
+    btnNo: 'yep,i loke it :3',
+    question: 'we're more suited to being friends  ❤',
+    btnReply: 'Gửi cho bạn ❤',
+    reply: 'Uh, i think so... ',
+    mess: 'T biết mà 🥰.hiuhiuuu  😘😘',
+    messDesc: 'goodbye and we are friends ',
+    btnAccept: 'Okiiiii lun ❤',
+    messLink: 'http://fb.com' 
+}
